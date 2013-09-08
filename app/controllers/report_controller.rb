80c94c1d363c741bce2b7ce96ebbcf08a47a6fd4
Max <maxbrsv@mail.ru>
2013-09-08 15:23:26 +0300

    init

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -7,9 +7,9 @@ class ReportController < ApplicationController
     @move = MoneyMove.find_all_by_category_id(@categories, :order => "date")
   end
   def create
-    @date_to = Date.new(params[:date_to][:year].to_i, params[:date_to][:month].to_i, params[:date_to][:day].to_i)
-    @date_from = Date.new(params[:date_from][:year].to_i, params[:date_from][:month].to_i, params[:date_from][:day].to_i)
-    @date_q_to = params[:date_to][:year]+"."+params[:date_to][:month]+"."+params[:date_to][:day]
+    @date_to = Date.new(params[:date_to][:year].to_i, params[:date_to][:month].to_i, params[:date_to][:day].to_i) unless @date_to = Date.current
+    @date_from = Date.new(params[:date_from][:year].to_i, params[:date_from][:month].to_i, params[:date_from][:day].to_i) unless @date_from = Date.current
+    @date_q_to = params[:date_to][:year]+"."+params[:date_to][:month]+"."+params[:date_to][:day]+".23.59"
     @date_q_from = params[:date_from][:year]+"."+params[:date_from][:month]+"."+params[:date_from][:day]
     if(params[:category][:id]=="")
       @categories = Category.find(:all, :conditions => ["user_id = ?", current_user.id])
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,19 @@
 class WelcomeController < ApplicationController
   def index
+    @users_count = User.count
+    if(@users_count==0)
+      @in_per_user = 0
+      @ex_per_user = 0
+      @mid_in = 0
+      @mid_ex = 0
+    else
+      @in_users = Category.find_all_by_is_income(true)
+      @ex_uses = Category.find_all_by_is_income(false)
+      @in_per_user = (@in_users.count / @users_count.to_f).round 2
+      @ex_per_user = (@ex_uses.count / @users_count.to_f).round 2
+      @mid_in = (MoneyMove.find_all_by_category_id(@in_users).sum(&:price) / MoneyMove.find_all_by_category_id(@in_users).count).round 2
+      @mid_ex = (MoneyMove.find_all_by_category_id(@ex_uses).sum(&:price) / MoneyMove.find_all_by_category_id(@ex_uses).count).round 2
+    end
   end
   def new
   end
