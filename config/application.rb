c838f02099ed38696df5102ebfb67b2c6e9760b9
Max <maxbrsv@mail.ru>
2013-09-08 18:34:26 +0300

    init

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module Pei
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.initialize_on_precompile = true
+    config.assets.precompile += ['application.css']
   end
 end
