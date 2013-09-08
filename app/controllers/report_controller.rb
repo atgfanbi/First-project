5272ace0b37ef8797f63ab4819d9d9f8c67cdb86
Max <maxbrsv@mail.ru>
2013-09-08 17:53:13 +0300

    init

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -5,6 +5,7 @@ class ReportController < ApplicationController
     @date_from = Time.current
     @categories = Category.find(:all, :conditions => ["user_id = ?", current_user.id])
     @move = MoneyMove.find_all_by_category_id(@categories, :order => "date")
+    correct_prices
   end
   def create
     @date_to = Date.new(params[:date_to][:year].to_i, params[:date_to][:month].to_i, params[:date_to][:day].to_i) unless @date_to = Date.current
@@ -18,6 +19,17 @@ class ReportController < ApplicationController
       @categories = Category.find(:all, :conditions => ["user_id = ? AND id = ?", current_user.id, params[:category][:id]])
     end
     @move = MoneyMove.find_all_by_category_id(@categories, :conditions => ["date >= ? AND date <=?", @date_q_from, @date_q_to], :order => "date")
+    correct_prices
     render "index"
+  end
+
+  private
+  def correct_prices
+    @move.each do |move|
+      if(!move.category.is_income)
+        move.price = -move.price
+      end
+    end
+    @total = @move.sum(&:price).round 2
   end
 end
