ae1483cbada0f255e656d3d146929cdcb81580b2
Max <maxbrsv@mail.ru>
2013-09-08 17:11:45 +0300

    init

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -11,8 +11,8 @@ class WelcomeController < ApplicationController
       @ex_uses = Category.find_all_by_is_income(false)
       @in_per_user = (@in_users.count / @users_count.to_f).round 2
       @ex_per_user = (@ex_uses.count / @users_count.to_f).round 2
-      @mid_in = (MoneyMove.find_all_by_category_id(@in_users).sum(&:price) / MoneyMove.find_all_by_category_id(@in_users).count).round 2
-      @mid_ex = (MoneyMove.find_all_by_category_id(@ex_uses).sum(&:price) / MoneyMove.find_all_by_category_id(@ex_uses).count).round 2
+      @mid_in = (MoneyMove.find_all_by_category_id(@in_users).sum(&:price) / MoneyMove.find_all_by_category_id(@in_users).count).round 2 unless @mid_in = 0
+      @mid_ex = (MoneyMove.find_all_by_category_id(@ex_uses).sum(&:price) / MoneyMove.find_all_by_category_id(@ex_uses).count).round 2 unless @mid_ex = 0
     end
   end
   def new
